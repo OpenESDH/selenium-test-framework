155434c0313123d3b9e6d302f883461606ae7a46
Thomas Kristensen <thomas@magenta.dk>
2015-06-24 17:04:02 +0200

    Moved to connect to share on port 8081

--- src/main/java/dk/magenta/share/selenium/framework/pages/BasePage.java
+++ src/main/java/dk/magenta/share/selenium/framework/pages/BasePage.java
@@ -21,7 +21,7 @@ import static org.junit.Assert.assertNotNull;
  */
 public abstract class BasePage {
 
-	public static final String BASE_URL = "http://localhost:8080/share";
+	public static final String BASE_URL = "http://localhost:8081/share";
 
 	/**
 	 * Headermenu item "Cases"
